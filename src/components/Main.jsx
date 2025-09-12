9bf4f7e1caf8c385ecfedbb6246f317e993a3dac
KMinHyun <kimdennis264@gmail.com>
2025-09-12 11:14:20 +0900

    250912 FESTIVAL_MAIN01 작업 완료

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import titleImg from '../assets/traditional_village_1.png';
+import './Main.css';
+
+
+function Main() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <img className='title-img' onClick={() => { navigate('/festivals') }} src={titleImg} alt="대문" />
+    </>
+  )
+}
+
+export default Main;
